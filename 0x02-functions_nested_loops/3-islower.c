3746bb5fb8b896d0a5dbf077e8df71f216864947
ZorEl212 <yeabsirad212@gmail.com>
2023-02-22 00:03:22 -0800

    Lowercase checker

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _islower - check for lowercase
+ * @zor: take char to be evaluated
+ *
+ * Return: 1 (success)
+ *         0 (not a lowercase)
+ */
+
+int _islower(int zor)
+{
+	if (islower(zor) != 0)
+		return (1);
+	return (islower(zor));
+}
