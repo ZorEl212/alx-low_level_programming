efff65c55f2ad6a2baddf31ce0e8c68e475d93e1
ZorEl212 <yeabsirad212@gmail.com>
2023-02-22 06:29:24 -0800

    Absolute value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include<stdlib.h>
+#include "main.h"
+
+/**
+ * int_abs -evaluate absolute value of an integer
+ * @num: take integer
+ *
+ * Return: (absolute value)
+ */
+
+int int_abs(int num)
+{
+	return (abs(num));
+}
