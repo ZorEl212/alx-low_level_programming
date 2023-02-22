85877869df6bfafe328b3e77f17463ea72491eca
ZorEl212 <yeabsirad212@gmail.com>
2023-02-21 22:55:24 -0800

    Loop it 10  times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - print lowercase alphabets 10 times
+ *
+ * Return: Null
+ */
+
+void print_alphabet_x10(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+
+		print_alphabet();
+}
