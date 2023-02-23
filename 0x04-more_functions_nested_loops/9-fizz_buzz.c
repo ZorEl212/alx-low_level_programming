fcd72bcf0622fac786553592e9258119908b0b5f
ZorEl212 <yeabsirad212@gmail.com>
2023-02-23 11:59:44 -0800

    Remove unused variables

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -8,12 +8,12 @@
 
 int main(void)
 {
-	int a, b, c;
+	int a;
 
 	for (a = 1; a < 100; a++)
 	{
 		if (a % 15 == 0)
-			printf("Fizz-Buzz ");
+			printf("FizzBuzz ");
 		else if (a % 5 == 0)
 			printf("Buzz ");
 		else if (a % 3 == 0)
