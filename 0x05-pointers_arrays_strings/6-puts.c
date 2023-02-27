a225eff0cba5190e3b8e1243ca9db009093735ad
ZorEl212 <yeabsirad212@gmail.com>
2023-02-27 05:52:06 -0800

    Half-life

---
+++ 0x05-pointers_arrays_strings/6-puts.c
@@ -0,0 +1,16 @@
+#include <string.h>
+/**
+ * puts2 - print skipping one element
+ * @str: string to evaluate
+ *
+ * Return: Null
+ */
+
+void puts2(char *str)
+{
+	int x, len;
+
+	len = strlen(str);
+	for (x = 0; x < len ; x += 2)
+		_putchar(str[x]);
+}
