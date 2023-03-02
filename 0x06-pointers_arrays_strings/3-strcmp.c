ef756227324a1f7aa1be8c6881da26569a3d964a
ZorEl212 <yeabsirad212@gmail.com>
2023-03-02 06:24:03 -0800

    Compare characters

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * _strcmp - compare two strings
+ * @s1: string 1
+ * @s2: string 2
+ *
+ * Return: 0(success)
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	int i = 0;
+
+	while (s1[i] != '\0' && s2[i] != '\0')
+	{
+		if (s1[i] < s2[i])
+			return (-1);
+		else if (s1[i] > s2[i])
+			return (1);
+	i++;
+	}
+	if (s1[i] == '\0' && s2[i] == '\0')
+		return (0);
+	else if (s1[i] == '\0')
+		return (-1);
+	else
+		return (1);
+}
