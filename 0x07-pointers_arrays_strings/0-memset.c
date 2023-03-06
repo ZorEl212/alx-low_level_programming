97a3ab360dea5c941ae052b38a4fc24a2a991703
ZorEl212 <yeabsirad212@gmail.com>
2023-03-06 07:13:07 -0800

    memset replica

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _memset - fill memory with a constant byte
+ * @s: string
+ * @b: constant byte to fill
+ * @n: byte(s) to be filled
+ *
+ * Return: Pointer to string s.
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	s[i] = b;
+
+	return (s);
+}
---
+++ 0x07-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+#Even more arrays,strings and pointers
