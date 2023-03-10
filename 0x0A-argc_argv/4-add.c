6608cc1ff7358e067f0ac6b7e6d588dde6ce0d50
ZorEl212 <yeabsirad212@gmail.com>
2023-03-10 11:27:46 -0800

    Add Numbers

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+* main - multiply two numbers
+* @argc: number of arguments
+* @argv: pointer to string of arguments
+*
+* Return: 1(Error) or 0(Success)
+*/
+
+int main(int argc, char *argv[])
+{
+	int sum = 0, i;
+
+	if (argc == 1)
+	{
+		printf("0\n");
+		return (0);
+	}
+	for (i = 1; i < argc; i++)
+	{
+		if (*argv[i] < 48 || *argv[i] > 57)
+		{
+			printf("Error\n");
+			return (1);
+		}
+	}
+
+	for (i = 1; i < argc; i++)
+	{
+		sum += atoi(argv[i]);
+	}
+	printf("%d\n", sum);
+	return (0);
+
+}
