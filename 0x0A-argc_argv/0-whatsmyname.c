57eda2230a3076c73a3757d5e40ec4e8bdfd620a
ZorEl212 <yeabsirad212@gmail.com>
2023-03-10 09:30:52 -0800

    Fix betty

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,9 +1,17 @@
 #include <stdio.h>
 
+/**
+ * main - print program name
+ * @argc: argument number
+ * @argv: pointer to string to argument
+ *
+ * Return: Success (0)
+ */
+
 int main(int argc, char *argv[])
 {
 	argc = argc; /* bypass unused var warning for gcc */
 
 	printf("%s\n", argv[0]);
-	return 0;
+	return (0);
 }
