473a7c3b4ff321f0d60c03dfe63186b3454ad06e
ZorEl212 <yeabsirad212@gmail.com>
2023-03-14 09:23:11 -0700

    array of strings

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * create_array - create array of string and fill it with spcifc char c
+ * @size: size of the array
+ * @c: character to fill
+ *
+ * Return: pointer to string or NULL
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	unsigned int i;
+	char *arr = (char *)malloc(sizeof(char) * size);
+
+	if (size == 0)
+		return (NULL);
+
+	for (i = 0; i < size; i++)
+	{
+		arr[i] = c;
+	}
+	return (arr);
+}
