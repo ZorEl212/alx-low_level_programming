f93b1e80670bba6d06ff087feaa9d5df22ab3066
ZorEl212 <yeabsirad212@gmail.com>
2023-03-16 09:40:16 -0700

    Allocate mem

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * malloc_checked - allocate memory address
+ * @b: size of memory to allocate
+ *
+ * Return: pointer to allocated mem address
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	void *ptr;
+
+	ptr = malloc(sizeof(b));
+	if (ptr == NULL)
+		exit(98);
+	return (ptr);
+}
