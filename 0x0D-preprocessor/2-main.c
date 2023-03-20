b00ae63e7d00dd00f5d04291cedbcf1252644ea9
ZorEl212 <yeabsirad212@gmail.com>
2023-03-20 09:47:43 -0700

    Print Source file name

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - print name of the file the program is compiled from
+ *
+ * Return: ) (Success)
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
