705949db5f3c657a5b8e8367c897d79da883c1dc
ZorEl212 <yeabsirad212@gmail.com>
2023-03-21 11:35:45 -0700

    Dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - description of dog
+ * @name: dog name
+ * @owner: owner of the dog
+ * @age: age of the dog
+ */
+
+
+struct dog
+{
+	char *name;
+	char *owner;
+	float age;
+};
+
+#endif
