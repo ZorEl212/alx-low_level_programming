d66bba97a2ef64868237ae251980c3a0ce9c9683
ZorEl212 <yabsirad212@gmail.com>
2023-07-06 15:14:04 -0400

    Fix check for NULL and empty key
    fix sigfault when hashing function is called on empty or null key

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -12,11 +12,12 @@
 
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	unsigned long int index = key_index((unsigned const char*) key, ht->size);
+	unsigned long int index;
 	hash_node_t *item;
 
-	if (key == NULL)
+	if (key == NULL || strlen(key) == 0)
 		return (0);
+	index = key_index((unsigned const char*) key, ht->size);
 
 	item = malloc(sizeof(hash_node_t));
 	if (item == NULL)
