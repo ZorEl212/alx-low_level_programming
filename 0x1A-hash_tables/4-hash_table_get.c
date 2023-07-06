8fd1f582d6c168a5a6eea470f81dc19bd58314c8
ZorEl212 <yabsirad212@gmail.com>
2023-07-06 14:39:33 -0400

    Function to Fetch a value for matching key

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,28 @@
+#include "hash_tables.h"
+
+
+/**
+ * hash_table_get - fetch a value based on matching key
+ * @ht: hash table to search in
+ * @key: key string
+ *
+ * Return: return a fetched value
+ */
+
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	unsigned long int index = key_index((const unsigned char *)key, ht->size);
+	hash_node_t *head = ht->array[index];
+
+	while (head != NULL)
+	{
+		if (strcmp(head->key, key) == 0 && head->value != NULL)
+		{
+			return (head->value);
+		}
+
+		head = (head->next);
+	}
+
+	return (NULL);
+}
