d483dca31244eed7635a919bc5261fc96472b6eb
ZorEl212 <yabsirad212@gmail.com>
2023-07-07 11:32:26 -0400

    Delete a table

---
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -0,0 +1,21 @@
+#include "hash_tables.h"
+
+void hash_table_delete(hash_table_t *ht)
+{
+    unsigned long int i;
+    for (i = 0; i < ht->size; i++)
+{
+    hash_node_t *current = ht->array[i];
+    while (current != NULL)
+    {
+        hash_node_t *temp = current;
+        current = current->next;
+        free(temp->key);
+        free(temp->value);
+        free(temp);
+    }
+}
+
+free(ht->array);
+free(ht);
+}
