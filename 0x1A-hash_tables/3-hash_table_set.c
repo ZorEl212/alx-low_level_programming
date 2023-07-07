1c566a43d8c61e42e9683fe1e19a6735d7846427
ZorEl212 <yabsirad212@gmail.com>
2023-07-07 10:12:41 -0400

    Updete a node if matching key is found
    split function to comply with betty style

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,6 +1,33 @@
 #include "hash_tables.h"
 
 
+hash_node_t *new_node(const char *key, const char *value)
+{
+	hash_node_t *item;
+	char *val_cpy;
+	char *key_cpy;
+	val_cpy = strdup(value);
+	key_cpy = strdup(key);
+
+	if (val_cpy == NULL || key_cpy == NULL)
+	{
+		free(val_cpy);
+		free(key_cpy);
+		return (NULL);
+	}
+
+	item = malloc(sizeof(hash_node_t));
+	if (item == NULL)
+		return (0);
+
+	item->key = key_cpy;
+	item->value = val_cpy;
+
+	return (item);
+
+}
+
+
 /**
  * hash_table_set - add a node on the table based on index
  * @ht: hash table to add to
@@ -13,27 +40,18 @@
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	unsigned long int index;
-	hash_node_t *item;
-	char *val_cpy;
-	char *key_cpy;
+	hash_node_t *current, *item;
 
 	if (key == NULL || strlen(key) == 0 || ht == NULL
 	|| ht->array == NULL || ht->size == 0)
 		return (0);
-	val_cpy = strdup(value);
-	key_cpy = strdup(key);
+
 	index = key_index((unsigned const char*) key, ht->size);
-
-	item = malloc(sizeof(hash_node_t));
-	if (item == NULL)
-		return (0);
-
-	item->key = key_cpy;
-	item->value = val_cpy;
 
 	if (ht->array == NULL)
 		return (0);
 
+	item = new_node(key, value);
 	if (ht->array[index] == NULL)
 	{
 		ht->array[index] = item;
@@ -43,6 +61,16 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 
 	else if (ht->array[index] != NULL)
 	{
+		current = ht->array[index];
+		while(current != NULL)
+		{
+			if (strcmp(key, current->key) == 0)
+			{
+				current->value = strdup(item->value);
+				return (1);
+			}	
+			current = current->next;
+		}
 		item->next = ht->array[index];
 		ht->array[index] = item;
 
