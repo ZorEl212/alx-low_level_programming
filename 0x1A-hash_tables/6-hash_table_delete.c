5f20093bdb2c6ffc61122d829f99ec50d9ec468e
ZorEl212 <yabsirad212@gmail.com>
2023-07-07 12:05:49 -0400

    Add extra check case

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -13,7 +13,7 @@ void hash_table_delete(hash_table_t *ht)
 	unsigned long int i;
 	hash_node_t *current, *temp;
 
-	if (ht == NULL || ht->array == NULL)
+	if (ht == NULL || ht->array == NULL || ht->size == 0)
 		return;
 
 	for (i = 0; i < ht->size; i++)
