3ec44b201d9fadcd1020031fa317ecb4fd5b4bb7
ZorEl212 <yabsirad212@gmail.com>
2023-07-07 11:38:22 -0400

    Fix betty

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -1,21 +1,31 @@
 #include "hash_tables.h"
 
+
+/**
+ * hash_table_delete - delete a hash table
+ * @ht: hash table to delete
+ *
+ * Return: Nothing
+ */
+
 void hash_table_delete(hash_table_t *ht)
 {
-    unsigned long int i;
-    for (i = 0; i < ht->size; i++)
-{
-    hash_node_t *current = ht->array[i];
-    while (current != NULL)
-    {
-        hash_node_t *temp = current;
-        current = current->next;
-        free(temp->key);
-        free(temp->value);
-        free(temp);
-    }
-}
+	unsigned long int i;
+	hash_node_t *current, *temp;
 
-free(ht->array);
-free(ht);
+	for (i = 0; i < ht->size; i++)
+	{
+		current = ht->array[i];
+		while (current != NULL)
+		{
+			temp = current;
+			current = current->next;
+			free(temp->key);
+			free(temp->value);
+			free(temp);
+		}
+	}
+
+	free(ht->array);
+	free(ht);
 }
