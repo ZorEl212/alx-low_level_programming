f8e1421c97cd985ac76dd15c840c3ffb89c62d52
ZorEl212 <yabsirad212@gmail.com>
2023-07-07 11:01:27 -0400

    Fix mixed declarations

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -11,10 +11,12 @@
 
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
+	unsigned long int index;
+	hash_node_t *head;
 	if (ht == NULL || ht->array == NULL || key == NULL)
 		return (NULL);
-	unsigned long int index = key_index((const unsigned char *)key, ht->size);
-	hash_node_t *head = ht->array[index];
+	index = key_index((const unsigned char *)key, ht->size);
+	head = ht->array[index];
 
 	while (head != NULL)
 	{
