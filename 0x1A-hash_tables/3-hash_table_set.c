bcef5715010cf76e8b6b2ccd87db5422bd58f3c3
ZorEl212 <yabsirad212@gmail.com>
2023-07-07 08:56:59 -0400

    Add some checks

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -14,17 +14,21 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	unsigned long int index;
 	hash_node_t *item;
-	char *val_cpy = strdup(value);
+	char *val_cpy;
+	char *key_cpy;
 
-	if (key == NULL || strlen(key) == 0 || ht == NULL)
+	if (key == NULL || strlen(key) == 0 || ht == NULL
+	|| ht->array == NULL || ht->size == 0)
 		return (0);
+	val_cpy = strdup(value);
+	key_cpy = strdup(key);
 	index = key_index((unsigned const char*) key, ht->size);
 
 	item = malloc(sizeof(hash_node_t));
 	if (item == NULL)
 		return (0);
 
-	item->key = (char *) key;
+	item->key = key_cpy;
 	item->value = val_cpy;
 
 	if (ht->array == NULL)
