bffed783b64dd115916282358cf144175d5d5a2c
ZorEl212 <yabsirad212@gmail.com>
2023-09-08 02:42:55 -0400

    Print a hash table like a python directory hash_tables.h - add function prototype and bool header 5-hash_table_print.c - func to print he ht

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,34 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_print - print a hash table like a python directory
+ * @ht: hash table to print
+ *
+ * Return: Nothing
+*/
+void hash_table_print(const hash_table_t *ht)
+{
+	hash_node_t *current;
+	unsigned long int i;
+	bool printed = false;
+
+	if (ht == NULL || ht->array == NULL)
+		return;
+
+	putchar('{');
+	for (i = 0; i < ht->size; i++)
+	{
+		if (ht->array[i] == NULL)
+			continue;
+		current = ht->array[i];
+		while (current != NULL)
+		{
+			if (printed == true)
+				printf(", ");
+			printf("'%s': '%s'", current->key, current->value);
+			printed = true;
+			current = current->next;
+		}
+	}
+	putchar('}');
+}
