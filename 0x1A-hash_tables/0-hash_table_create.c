5e073f70246f3a803541ae5d627de098f0e0500f
ZorEl212 <yabsirad212@gmail.com>
2023-09-08 01:52:47 -0400

    FIX: wrong calloc size for ht->array

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -20,7 +20,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 		return (NULL);
 
 	table->size = size;
-	table->array = calloc(size, sizeof(hash_node_t));
+	table->array = calloc(size, sizeof(hash_node_t *));
 	if (table->array == NULL)
 		return (NULL);
 	for (i = 0; i > table->size; i++)
