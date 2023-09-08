a99ace42836ecb3ce439ce030600f99fbb636758
ZorEl212 <yabsirad212@gmail.com>
2023-09-08 03:42:38 -0400

    FIX: add some validity checks

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -12,6 +12,9 @@ void hash_table_delete(hash_table_t *ht)
 	unsigned long int i;
 	hash_node_t *head, *temp;
 
+	if (ht == NULL || ht->array == NULL || ht->size == 0)
+		return;
+
 	for (i = 0; i < ht->size; i++)
 	{
 		head = ht->array[i];
